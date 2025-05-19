abfbbd961e24f8ea80256faa85590bc657688ce9
h23jonca <h23jonca@du.se>
2025-05-19 16:44:56 +0200

    onödig kod?

--- frontend/static/embed/embed.js
+++
@@ -1,9 +0,0 @@
-// frontend/static/embed/embed.js
-import '/static/js/CameraLayer.js';
-import '/static/js/RoadworkLayer.js';
-import '/static/js/AccidentLayer.js';
-import '/static/js/MapComponent.js';
-
-// Inget mer behövs här — MapComponent registrerar sig själv
-
-  
